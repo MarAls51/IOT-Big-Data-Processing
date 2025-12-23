09418a8595253ba5bb4dce4a2c7314d698a984db
MarAls51 <74230641+MarAls51@users.noreply.github.com>
2025-12-22 23:39:44 -0500

    Update silver_layer.py

--- transformations/silver_layer.py
+++ transformations/silver_layer.py
@@ -1,6 +1,8 @@
 import dlt
-from pyspark.sql.functions import concat_ws, to_timestamp, col, trim, row_number
-from pyspark.sql.window import Window
+import mlflow
+from pyspark.sql.functions import concat_ws, to_timestamp, col, trim, struct
+
+predict_anomaly_udf = mlflow.pyfunc.spark_udf(spark, model_uri="models:/home_security_model/production")
 
 @dlt.table(
     name="silver_iot_events",
@@ -10,6 +12,7 @@ from pyspark.sql.window import Window
         "delta.autoOptimize.optimizeWrite": "true"
     }
 )
+
 def silver_iot_events():
     df = dlt.read_stream("bronze_iot_events")
     
@@ -19,3 +22,18 @@ def silver_iot_events():
     ).withColumn("state", trim(col("state"))).drop("time")
     
     return df.dropDuplicates(["room", "event_datetime"])
+
+@dlt.table(
+    name="security_alerts",
+    comment="Real-time security and health anomalies detected via ML inference",
+)
+
+def security_alerts():
+    df = dlt.read_stream("silver_iot_events")
+    
+    df_with_predictions = df.withColumn(
+        "is_anomaly", 
+        predict_anomaly_udf(struct("room", "state"))
+    )
+    
+    return df_with_predictions.filter(col("is_anomaly") == "ON")
